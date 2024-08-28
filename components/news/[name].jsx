9b0357a8476b0ed8458df072c1401e66afb603f3
Ervand <vip.ervand.88@gmail.com>
2024-08-28 16:16:09 +0400

    Changes

--- components/news/[name].jsx
+++ components/news/[name].jsx
@@ -1,79 +1,77 @@
-import React, {useEffect} from 'react';
-import Head from "next/head";
-import Image from "next/image";
-import SmallItem from "@/components/news/smallitem";
-import App from "@/components/layouts/app";
-import {useRouter} from "next/router";
-import {useDispatch, useSelector} from "react-redux";
-import {getNewsById, getRandNews} from "@/store/news/actions";
-import {Skeleton} from "antd";
-import Link from "next/link";
-import {FacebookIcon, FacebookShareButton} from "react-share";
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
-import { Helmet } from 'react-helmet';
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import SmallItem from '@/components/news/smallitem';
+import App from '@/components/layouts/app';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import { getNewsById, getRandNews } from '@/store/news/actions';
+import { Skeleton } from 'antd';
+import Link from 'next/link';
+import { FacebookIcon, FacebookShareButton } from 'react-share';
 
 const Name = () => {
-
-
   const router = useRouter();
-  const {name} = router.query;
+  const { name } = router.query;
   const dispatch = useDispatch();
   const news = useSelector((state) => state?.news?.selectedNews);
   const randNews = useSelector((state) => state?.news?.news);
   const isFetching = useSelector((state) => state?.news?.isFetching);
-  const pageUrl = `https://www.182dproc.am/news/${name}`
+  const pageUrl = `https://www.182dproc.am/news/${name}`;
   const imageUrl = news?.avatar
     ? `${process.env.IMAGE_URL}${news.avatar}`
-    : "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp";
+    : 'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp';
 
   useEffect(() => {
-    dispatch(getNewsById.request({id: name}));
+    dispatch(getNewsById.request({ id: name }));
     dispatch(getRandNews.request());
   }, [dispatch, name]);
-  const trimmedContent = news?.content ? news.content.substring(0, 150) + "..." : "";
+
+  const trimmedContent = news?.content ? news.content.substring(0, 150) + '...' : '';
 
   return (
     <>
+      <Head>
+        <meta property="og:title" content={news?.title || 'Default Title'} />
+        <meta property="og:description" content={trimmedContent || 'Default description'} />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:type" content="article" />
+      </Head>
       <App>
-        <Helmet>
-          <meta property="og:title" content={news?.title}/>
-          <meta property="og:description" content={news?.description || 'Default description'}/>
-          <meta property="og:image" content={imageUrl}/>
-          <meta property="og:url" content={`https://182school.geeklab.am/storage/news/August2024/AW3WviGeSDxTkbG3evtf.jpeg`}/>
-          <meta property="og:type" content="website"/>
-        </Helmet>
-        <h1>{imageUrl}</h1>
-
-        <div className='w-11/12 h-max  justify-between flex m-auto  newsName '>
-          <div className='w-2/3 h-max newsNameFirst '>
+        <div className='w-11/12 h-max justify-between flex m-auto newsName'>
+          <div className='w-2/3 h-max newsNameFirst'>
             <Skeleton loading={isFetching} active>
               <div className='w-11/12 m-auto mt-6 mb-10'>
                 <p className='text-xl italic'>13/06/2024</p>
               </div>
               <div className='w-11/12 m-auto'>
-                <h1 className='mb-10 text-2xl '>{news?.title}</h1>
+                <h1 className='mb-10 text-2xl'>{news?.title}</h1>
               </div>
               <div className='w-11/12 m-auto h-3/6'>
-                <Image src={process.env.IMAGE_URL + news?.avatar} className='w-full h-full object-cover' alt={"Image"}
-                       width={1000}
-                       height={1000}/>
+                <Image
+                  src={process.env.IMAGE_URL + news?.avatar}
+                  className='w-full h-full object-cover'
+                  alt={'Image'}
+                  width={1000}
+                  height={1000}
+                />
               </div>
               <div className='w-11/12 m-auto h-auto mt-6'>
-                <p dangerouslySetInnerHTML={{__html: news?.content}}></p>
+                <p dangerouslySetInnerHTML={{ __html: news?.content }}></p>
               </div>
               <div className="w-11/12 mt-6 ml-10 mb-10">
                 <FacebookShareButton
-                  url={`https://www.182dproc.am/news/${name}`}
+                  url={pageUrl}
                   quote={news?.title}
                   hashtag={'#kd'}
                 >
-                  <FacebookIcon size={32} round={true}/>
+                  <FacebookIcon size={32} round={true} />
                 </FacebookShareButton>
               </div>
             </Skeleton>
           </div>
-          <div className=' pl-10 w-2/6 h-auto smallItem'>
-
+          <div className='pl-10 w-2/6 h-auto smallItem'>
             <div className='mt-6 mb-10'>
               <h1 className='text-xl italic'>Վերջին նորությունները</h1>
             </div>
@@ -81,7 +79,7 @@ const Name = () => {
               <>
                 {randNews?.map((item) => (
                   <Link key={item.id} href={`/news/${item.id}`}>
-                    <SmallItem item={item}/>
+                    <SmallItem item={item} />
                   </Link>
                 ))}
               </>
